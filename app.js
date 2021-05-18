0bda019da9231f60f7420804f59daab879260d88
Kunal <kunalnandwana1999@gmail.com>
2021-05-18 09:38:17 +0530

    Done

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
+// var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/klingon.json"
 
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something wrong with server try again after sometime")
+}
 function clickHandler(){
     
-    outputDiv.innerText = "jsndja"+ txtInput.value;
+    // outputDiv.innerText = "jsndja"+ txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 };
+
+
 
 btnTranslate.addEventListener("click", clickHandler)
